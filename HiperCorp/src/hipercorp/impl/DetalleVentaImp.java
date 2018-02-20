d5e2ae0af657d8c8c7820463abd9ac7a6f1a5b71
Usuario <Usuario@DESKTOP-B6KPNQC>
2018-02-20 13:01:27 -0500

    Diseño

--- HiperCorp/src/hipercorp/impl/DetalleVentaImp.java
+++ HiperCorp/src/hipercorp/impl/DetalleVentaImp.java
@@ -16,23 +16,23 @@ import java.util.List;
  *
  * @author Usuario
  */
-public class DetalleVentaImp implements IDetalleVenta{
-    
+public class DetalleVentaImp implements IDetalleVenta{ 
     @Override
     public int insertar(DetalleVenta detalle_venta) throws Exception {
        int numFilasAfectadas = 0;
-       String sql = "insert into proveedor  values (?,?,?,?,?)";
-        List<Parametro> lstPar = new ArrayList<>();
-        lstPar.add(new Parametro(1, detalle_venta.getCodDetalleVenta()));
-        lstPar.add(new Parametro(2, detalle_venta.getCodFacturaVenta()));
-        lstPar.add(new Parametro(3, detalle_venta.getCodProducto()));
-        lstPar.add(new Parametro(4, detalle_venta.getCantidad()));
-        lstPar.add(new Parametro(5, detalle_venta.getPrecioT()));
+       String sql = "insert into DetalleVenta values (?,?,?,?,?)";
+        List<Parametro> lst;
+        lst = new ArrayList<>();
+        lst.add(new Parametro(1, detalle_venta.getCodDetalleVenta()));
+        lst.add(new Parametro(2, detalle_venta.getCodFacturaVenta()));
+        lst.add(new Parametro(3, detalle_venta.getCodProducto()));
+        lst.add(new Parametro(4, detalle_venta.getCantidad()));
+        lst.add(new Parametro(5, detalle_venta.getPrecioT()));
         Conexion con = null;
         try {
             con = new Conexion();
             con.conectar();
-            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+            numFilasAfectadas = con.ejecutaComando(sql, lst);
         } catch (Exception e) {
             throw e;
         } finally {
@@ -73,7 +73,7 @@ public class DetalleVentaImp implements IDetalleVenta{
     @Override
     public int eliminar(DetalleVenta detalle_venta) throws Exception {
         int numFilasAfectadas = 0;
-         String sql = "DELETE FROM DetalleVenta where CoddetalleVenta=?";
+         String sql = "DELETE FROM DetalleVenta where CodDetalleVenta=?";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, detalle_venta.getCodDetalleVenta()));
         lstPar.add(new Parametro(2, detalle_venta.getCodFacturaVenta()));
@@ -130,17 +130,31 @@ public class DetalleVentaImp implements IDetalleVenta{
     }
     
     @Override
-    public List<IDetalleVenta> obtener() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<DetalleVenta> obtener() throws Exception {
+         List<DetalleVenta > lst = new ArrayList<>();
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql,null);
+            DetalleVenta detalle_venta=null;
+            while (rst.next()) {
+                detalle_venta = new DetalleVenta();
+                detalle_venta.setCodDetalleVenta(rst.getInt(1));
+                detalle_venta.setCodFacturaVenta(rst.getInt(2));
+                detalle_venta.setCodProducto(rst.getInt(3));
+                detalle_venta.setCantidad(rst.getInt(4));
+                detalle_venta.setPrecioT(rst.getInt(5));   
+                
+                lst.add(detalle_venta);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        
+        return lst;
     }
-
-    @Override
-    public int insertar(IDetalleVenta detalle_venta) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-
-   
-    
-    
-}
+  }
