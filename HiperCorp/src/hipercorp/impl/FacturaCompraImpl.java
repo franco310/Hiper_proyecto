4c3115f66aa870111a333a789dec9bc75b7ef62f
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-19 23:32:35 -0500

    MarcoAmancha

--- HiperCorp/src/hipercorp/impl/FacturaCompraImpl.java
+++ HiperCorp/src/hipercorp/impl/FacturaCompraImpl.java
@@ -1,14 +1,145 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hipercorp.impl;
 
-/**
- *
- * @author Usuario
- */
-public class FacturaCompraImpl {
+import hipercorp.accesodatos.Conexion;
+import hipercorp.accesodatos.Parametro;
+import hipercorp.dao.IFacturaCompra;
+import hipercorp.dao.IProveedor;
+import hipercorp.entidades.FacturaCompra;
+import hipercorp.entidades.Proveedor;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacturaCompraImpl implements IFacturaCompra{
+    
+     @Override
+    public int insertar(FacturaCompra facturacompra) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "insert into FacturaCompra  values "
+                +"(?,?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, facturacompra.getIdFacturaCompra()));
+        lstPar.add(new Parametro(2, facturacompra.getFecha()));
+        lstPar.add(new Parametro(3, facturacompra.getProveedor().getIdProveedor()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int modificar(FacturaCompra facturacompra) throws Exception {
+        int numFilasAfectadas = 0;
+        String sql = "UPDATE FacturaCompra"
+                + "   SET idFacturaCompra=?, fecha=?, idProveedor=?"
+                + " where idFacturaCompra=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, facturacompra.getIdFacturaCompra()));
+        lstPar.add(new Parametro(2, facturacompra.getFecha()));
+        lstPar.add(new Parametro(3, facturacompra.getProveedor().getIdProveedor()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public int eliminar(FacturaCompra facturacompra) throws Exception {
+        int numFilasAfectadas = 0;
+         String sql = "DELETE FROM FacturaCompra  where idFacturaCompra=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, facturacompra.getIdFacturaCompra()));       
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutaComando(sql, lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilasAfectadas;
+    }
+
+    @Override
+    public FacturaCompra obtener(int idFacturaCompra) throws Exception {
+        FacturaCompra facturacompra = null;
+        String sql = "SELECT idFacturaCompra, fecha,"
+                + " idProveedor   FROM FacturaCompra where idFacturaCompra=?";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, idFacturaCompra));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, lstPar);
+            while (rst.next()) {
+                facturacompra = new FacturaCompra();
+                facturacompra.setIdFacturaCompra(rst.getInt(1));
+                facturacompra.setFecha(rst.getDate(5));
+                IProveedor proveedordao = new ProveedorImpl();
+                Proveedor proveedor = proveedordao.obtener(rst.getInt(10));
+                facturacompra.setProveedor(proveedor);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return facturacompra;
+    }
+
+    @Override
+    public List<FacturaCompra> obtener() throws Exception {
+        List<FacturaCompra> lista = new ArrayList<>();
+         String sql = "SELECT idFacturaCompra, fecha ,"
+                 + "idProveedor   FROM FacturaCompra ";        
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutaQuery(sql, null);
+            FacturaCompra facturacompra=null;
+            while (rst.next()) {
+                facturacompra = new FacturaCompra();
+                facturacompra.setIdFacturaCompra(rst.getInt(1));
+                facturacompra.setFecha(rst.getDate(5));
+                IProveedor proveedordao = new ProveedorImpl();
+                Proveedor proveedor = proveedordao.obtener(rst.getInt(10));
+                facturacompra.setProveedor(proveedor);
+                lista.add(facturacompra);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return lista;
+    }
     
 }
+
