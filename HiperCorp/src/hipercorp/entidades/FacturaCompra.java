13caea01d16417fb2d4678a74677f28f63775626
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-19 23:31:21 -0500

    MarcoAmancha

--- HiperCorp/src/hipercorp/entidades/FacturaCompra.java
+++ HiperCorp/src/hipercorp/entidades/FacturaCompra.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hipercorp.entidades;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Date;
+
 public class FacturaCompra {
+    
+    private int idFacturaCompra;
+    private Date fecha;
+    private Proveedor proveedor;
+
+    public FacturaCompra() {
+    }
+
+    public FacturaCompra(int idFacturaCompra, Date fecha, Proveedor proveedor) {
+        this.idFacturaCompra = idFacturaCompra;
+        this.fecha = fecha;
+        this.proveedor = proveedor;
+    }
+
+    public int getIdFacturaCompra() {
+        return idFacturaCompra;
+    }
+
+    public void setIdFacturaCompra(int idFacturaCompra) {
+        this.idFacturaCompra = idFacturaCompra;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+    
+    
     
 }
