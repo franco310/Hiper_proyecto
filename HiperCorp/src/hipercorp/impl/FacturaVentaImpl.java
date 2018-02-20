eb1a0ed22ab1ae3d1d4ded7b78d578c43012f104
Segovia <Segovia@DESKTOP-2GC2MO3>
2018-02-20 12:30:37 -0500

    impl

--- HiperCorp/src/hipercorp/impl/FacturaVentaImpl.java
+++ HiperCorp/src/hipercorp/impl/FacturaVentaImpl.java
@@ -1,5 +1,3 @@
-
-
 package hipercorp.impl;
 
 import hipercorp.accesodatos.Conexion;
@@ -19,7 +17,6 @@ public class FacturaVentaImpl implements IFacturaVenta{
                 + "(?,?,?)";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, facturaventa.getIdFacturaVenta()));
-       // lstPar.add(new Parametro(2, facturaventa.getCliente().get));
         lstPar.add(new Parametro(3, facturaventa.getFecha()));
         lstPar.add(new Parametro(4, facturaventa.getCedula()));
     
@@ -42,11 +39,11 @@ public class FacturaVentaImpl implements IFacturaVenta{
     public int modificar(FacturaVenta facturaventa) throws Exception {
         int numFilasAfectadas = 0;
         String sql = "UPDATE facuraventa"
-                + "   SET idFacturaVenta=?, nombre=?, "
+                + "   SET idFacturaVenta=?,"
                 + " fecha=?,cedula=?,where idFacturaVenta=?";
         List<Parametro> lstPar = new ArrayList<>();
             lstPar.add(new Parametro(1, facturaventa.getIdFacturaVenta()));
-       // lstPar.add(new Parametro(2, facturaventa.getCliente().get));
+      
         lstPar.add(new Parametro(3, facturaventa.getFecha()));
         lstPar.add(new Parametro(4, facturaventa.getCedula()));
         Conexion con = null;
@@ -89,7 +86,7 @@ public class FacturaVentaImpl implements IFacturaVenta{
     public FacturaVenta obtener(int IdFacturaVenta) throws Exception {
         FacturaVenta facturaventa = null;
         String sql =  "UPDATE facuraventa"
-                + "   SET idFacturaVenta=?, nombre=?, "
+                + "   SET idFacturaVenta=? "
                 + " fecha=?,cedula=?,where idFacturaVenta=?";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, IdFacturaVenta));
@@ -100,10 +97,9 @@ public class FacturaVentaImpl implements IFacturaVenta{
             ResultSet rst = con.ejecutaQuery(sql, lstPar);
             while (rst.next()) {
                 facturaventa = new FacturaVenta();
-                facturaventa.setIdFacturaVenta(rst.getInt(1));
-               // facturaventasetNombre(rst.getString(2));
-                facturaventa.setFecha(rst.getDate(3));
-                facturaventa.setCedula(rst.getInt(4));
+              facturaventa.setIdFacturaVenta(rst.getInt(1));
+                facturaventa.setFecha(rst.getDate(2));
+                facturaventa.setCedula(rst.getInt(3));
                  
             }
         } catch (Exception e) {
@@ -118,7 +114,7 @@ public class FacturaVentaImpl implements IFacturaVenta{
     @Override
     public List<FacturaVenta> obtener() throws Exception {
         List<FacturaVenta> lista = new ArrayList<>();
-         String sql = "SELECT idFacturaVenta, nombre , fecha, cedula, FROM proveedor ";        
+         String sql = "SELECT idFacturaVenta , fecha, cedula, FROM proveedor ";        
         Conexion con = null;
         try {
             con = new Conexion();
@@ -128,9 +124,8 @@ public class FacturaVentaImpl implements IFacturaVenta{
             while (rst.next()) {
                 facturaventa = new FacturaVenta();
                facturaventa.setIdFacturaVenta(rst.getInt(1));
-              //  facturaventasetNombre(rst.getString(2));
-                facturaventa.setFecha(rst.getDate(3));
-                facturaventa.setCedula(rst.getInt(4));
+                facturaventa.setFecha(rst.getDate(2));
+                facturaventa.setCedula(rst.getInt(3));
                                
                 lista.add(facturaventa);
             }
