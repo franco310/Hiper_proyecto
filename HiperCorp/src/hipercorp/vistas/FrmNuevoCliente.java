ce8fc87417ee6d6114c1c56e5b007380b595c224
Francisco <Francisco@192.168.1.137>
2018-02-20 08:14:40 -0500

    actualizacion

---
+++ HiperCorp/src/hipercorp/vistas/FrmNuevoCliente.java
@@ -0,0 +1,149 @@
+
+package hipercorp.vistas;
+
+import hipercorp.dao.ICliente;
+import hipercorp.entidades.Cliente;
+import hipercorp.impl.ClienteImpl;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Francisco
+ */
+public class FrmNuevoCliente extends JInternalFrame {
+    
+    JLabel lblTitulo0;
+    JLabel lblcedula;
+    JLabel lblnombre;
+    JLabel lblapellido;
+    JLabel lbldireccion;
+    JLabel lbltelefono;
+    JLabel lblemail;
+    JLabel lblFecha;
+    
+    JTextField txtcedula;
+    JTextField txtnombre;
+    JTextField txtapellido;
+    JTextField txtdireccion;
+    JTextField txttelefono;
+    JTextField txtemail;
+    JTextField txtfecha;
+    
+    JButton btnLimpiar;
+    JButton btnAceptar;
+    JPanel pnlCentral;
+    JPanel pnlPie;
+    
+     public FrmNuevoCliente() {
+        this.setSize(300, 300);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        pnlCentral= new JPanel();
+        pnlPie= new JPanel();
+        pnlCentral.setLayout(new GridLayout(10, 2, 5, 5));
+        pnlPie.setLayout(new GridLayout(1,2,5,5));
+        
+        lblTitulo0 = new JLabel("Datos del Cliente");
+        
+        lblcedula= new JLabel("Cedula de Identidad:");
+        lblnombre= new JLabel("Nombre:");
+        lblapellido= new JLabel("Apelldio:");
+        lbldireccion= new JLabel("Direccion:");
+        lbltelefono= new JLabel("Telefono:");
+       lblemail= new JLabel("Email:");
+        lblFecha= new JLabel("Fecha de nacimiento:"); 
+
+        txtcedula = new JTextField(2);
+        txtnombre= new JTextField(2);
+        txtapellido= new JTextField(2);
+        txtdireccion= new JTextField(2);
+        txttelefono= new JTextField(2);
+        txtemail= new JTextField(2);
+        txtfecha= new JTextField(2);
+        
+        
+        btnLimpiar= new JButton("Limpiar");
+        btnAceptar= new JButton("Aceptar");
+        
+        pnlCentral.add(lblcedula);
+        pnlCentral.add(txtcedula);
+        pnlCentral.add(lblnombre);
+        pnlCentral.add(txtnombre);
+        pnlCentral.add(lblapellido);
+        pnlCentral.add(txtapellido);
+        pnlCentral.add(lbldireccion);
+        pnlCentral.add(txtdireccion);
+        pnlCentral.add(lbltelefono);
+        pnlCentral.add(txttelefono);
+        pnlCentral.add(lblemail);
+        pnlCentral.add(txtemail);
+        pnlCentral.add(lblFecha);
+        pnlCentral.add(txtfecha);       
+        btnAceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    btnAceptarActionListener(e);
+                } catch (Exception ex) {
+                    System.out.println("Error:"+ex.getMessage());
+                }
+            }
+        });
+        
+        pnlPie.add(btnLimpiar);
+        pnlPie.add(btnAceptar);
+        this.add(lblTitulo0, BorderLayout.NORTH);
+        this.add(pnlCentral, BorderLayout.CENTER);
+        this.add(pnlPie, BorderLayout.SOUTH);        
+    }
+    public static void main(String[] args) {
+        FrmNuevoCliente frmMenu= new FrmNuevoCliente();
+        frmMenu.setVisible(true);
+    } 
+    
+    public void btnAceptarActionListener(ActionEvent e){
+        ICliente clienteDao = new ClienteImpl();
+        Cliente cliente = new Cliente();
+        cliente.setCedula(Integer.parseInt(txtcedula.getText()));
+        cliente.setNombres(txtnombre.getText());
+        cliente.setApellidos(txtapellido.getText());
+        cliente.setDireccion(txtdireccion.getText());
+        cliente.setTelefono(txttelefono.getText());
+        cliente.setDireccion(txtemail.getText());
+      DateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+            try {
+                cliente.setFecha_Nace((Date) formatoFecha.parse(txtfecha.getText()));
+                
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this,"Error en la fecha!!",
+                "Error", JOptionPane.ERROR_MESSAGE);
+            };
+        
+        try {
+            if(clienteDao.insertar(cliente)>0){
+                JOptionPane.showMessageDialog(this,"Guaradado correctamente!!",
+                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+            }else{
+                JOptionPane.showMessageDialog(this,"Error desconocido!!",
+                "Transacción", JOptionPane.INFORMATION_MESSAGE);
+            }           
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this,"Error al guardar!!: " + ex.getMessage(),
+                "Error", JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+    }
+    
+}
