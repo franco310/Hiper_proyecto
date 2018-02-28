b43f9863ed0539f261258008a42cc6924953cd74
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-28 02:15:07 -0500

    MarcoAmancha

--- HiperCorp/src/hipercorp/dao/IUsuario.java
+++ HiperCorp/src/hipercorp/dao/IUsuario.java
@@ -6,6 +6,6 @@ public interface IUsuario {
     public int insertar(Usuario usuario) throws Exception;
     public int modificar(Usuario usuario) throws Exception;
     public int eliminar(Usuario usuario) throws Exception;
-    public Usuario obtener(int Codigo) throws Exception;
+    public Usuario obtener(int idUsuario) throws Exception;
     public List<Usuario> obtener() throws Exception;    
 }
