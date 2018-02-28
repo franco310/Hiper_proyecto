e378e774c58ecbc73a10338e94e897dfe871672a
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-28 00:16:40 -0500

    MarcoAmancha

--- HiperCorp/src/hipercorp/dao/IProducto.java
+++ HiperCorp/src/hipercorp/dao/IProducto.java
@@ -5,14 +5,10 @@ import hipercorp.entidades.Producto;
 import java.util.List;
 import java.util.*;
 
-/**
- *
- * @author Francisco
- */
 public interface IProducto {
     public int insertar(Producto producto) throws Exception;
     public int modificar(Producto producto) throws Exception;
     public int eliminar(Producto producto) throws Exception;
-    public Producto obtener(int codigoProducto) throws Exception;
+    public Producto obtener(int idProducto) throws Exception;
     public List<Producto> obtener() throws Exception;   
 }
